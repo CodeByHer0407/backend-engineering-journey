bbe5edd2d22a606a7554a46f88742223ba565a51
Saloni Azad <saloni.azad4799@gmail.com>
2026-06-27 00:11:19 +0530

    Production database change

--- 02_fastapi/TodoApp/database.py
+++ 02_fastapi/TodoApp/database.py
@@ -5,11 +5,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+SQLALCHEMY_DATABASE_URL = "postgresql://todoapp_db_r8j5_user:L4SPYjK52pJyNEur4SFZhwAwlSIlM3P3@dpg-d8vcco0g4nts738q1nk0-a.singapore-postgres.render.com/todoapp_db_r8j5"
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
 # SQLite database file
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
+#SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
 
 # Engine: responsible for connecting SQLAlchemy to the database
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+#engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
 # Session factory: creates a new database session for each request
 sessionLocal = sessionmaker(
