f07291c8ae5bc6f5d32716d778ee33a3eb9f2b3e
Saloni Azad <saloni.azad4799@gmail.com>
2026-06-27 00:16:01 +0530

    fix render issues

--- 02_fastapi/TodoApp/database.py
+++ 02_fastapi/TodoApp/database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABASE_URL = "postgresql://todoapp_db_r8j5_user:L4SPYjK52pJyNEur4SFZhwAwlSIlM3P3@dpg-d8vcco0g4nts738q1nk0-a.singapore-postgres.render.com/todoapp_db_r8j5"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 # SQLite database file
 #SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
